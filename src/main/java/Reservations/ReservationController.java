3f9814e38c47e5ab5b942776481ceaf8aa3eef04
YansLeem <yans1996@yandex.ru>
2019-02-15 02:47:17 +0300

    reservation v3

--- src/main/java/Reservations/ReservationController.java
+++ src/main/java/Reservations/ReservationController.java
@@ -1,20 +1,12 @@
 package Reservations;
 
 
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class ReservationController {
